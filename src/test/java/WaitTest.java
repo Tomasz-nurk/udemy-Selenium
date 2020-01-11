fb3f91f3b0c99b6de659b36c967ce056a749ab47
Tomasz-nurk <zsamotrunk@gmail.com>
2020-01-11 12:33:00 +0100

    Uncomment assertion

--- src/test/java/WaitTest.java
+++ src/test/java/WaitTest.java
@@ -7,15 +7,15 @@ import java.util.concurrent.TimeUnit;
 
 public class WaitTest extends BaseSeleniumTest {
     @Test
-    public void testWait() {
+    public void testWait() throws InterruptedException {
 
         driver.get("file:///C:/Users/Admin/Documents/zasoby/Waits.html");
         WebElement button = driver.findElement(By.id("clickOnMe"));
-        //button.click();
+        button.click();
         //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        //Thread.sleep(3000);
+        Thread.sleep(3000);
         WebElement paragraph = driver.findElement(By.xpath("/html/body/p"));
         System.out.println(paragraph.getText());
-        //Assert.assertEquals(paragraph.getText(), "Dopiero się pojawiłem!");
+        Assert.assertEquals(paragraph.getText(), "Dopiero się pojawiłem!");
     }
 }
